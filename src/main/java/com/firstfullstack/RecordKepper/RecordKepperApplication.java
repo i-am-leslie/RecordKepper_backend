ad55b1c72a0791456d8903dfcb09f7fbf0488085
leslie <leslieejeh@cmail.com>
2023-05-14 13:26:07 -0400

    id

--- src/main/java/com/firstfullstack/RecordKepper/RecordKepperApplication.java
+++ src/main/java/com/firstfullstack/RecordKepper/RecordKepperApplication.java
@@ -3,6 +3,7 @@ package com.firstfullstack.RecordKepper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class RecordKepperApplication {
 
--- src/main/java/com/firstfullstack/RecordKepper/controller/StudentController.java
+++ src/main/java/com/firstfullstack/RecordKepper/controller/StudentController.java
@@ -3,7 +3,9 @@ package com.firstfullstack.RecordKepper.controller;
 import com.firstfullstack.RecordKepper.model.Student;
 import com.firstfullstack.RecordKepper.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -24,9 +26,21 @@ public class StudentController {
 
     }
     //Yet to Implement
-    @GetMapping("/getStudent")
-    public String getStudent(@RequestParam("id") int id){
+    @GetMapping("/getStudent/{id}")
+    public String getStudent(@PathVariable int id){
+        if(studentService.getStudentInfo(id)==null){
+            throw new ResponseStatusException(HttpStatus.NO_CONTENT);
+        }
         return studentService.getStudentInfo(id).toString();
+
+    }
+    //Yet to implement
+    @DeleteMapping("/getStudent/{id}")
+    public String deleteStudent(@PathVariable int id){
+        if(studentService.getStudentInfo(id)==null){
+            throw new ResponseStatusException(HttpStatus.NO_CONTENT);
+        }
+        return "Student with id= " + id +" has been deleted" ;
 
     }
 
